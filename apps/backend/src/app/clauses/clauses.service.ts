35621d4d3efa4b5ba2137fb41c496ab51f745315
I. Aragao <iaragao@gmail.com>
2025-03-30 20:42:32 -0300

    add xmldom and parse the clauses to string in the backend

--- apps/backend/src/app/clauses/clauses.service.ts
+++ apps/backend/src/app/clauses/clauses.service.ts
@@ -1,11 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { ClausesRepository, Clause } from './clauses.repository';
+import { ClausesRepository } from './clauses.repository';
+import { DOMParser } from 'xmldom';
+import AdmZip from 'adm-zip';
 
 @Injectable()
 export class ClausesService {
-  constructor(private readonly clausesRepository: ClausesRepository) {}
+  private readonly parser: DOMParser;
 
-  findAll(): Clause[] {
-    return this.clausesRepository.findAll();
+  constructor(private readonly clausesRepository: ClausesRepository) {
+    this.parser = new DOMParser();
+  }
+
+  async findAll() {
+    const clauses = await this.clausesRepository.findAll();
+
+    return clauses.map((clause) => ({
+      ...clause,
+      content: this.convertBufferToString(clause.content),
+    }));
+  }
+
+  private convertBufferToString(buffer: Buffer): string {
+    const zip = new AdmZip(buffer);
+    const documentXml = zip.getEntry('word/document.xml');
+
+    if (!documentXml) {
+      throw new Error('No document.xml found in docx file');
+    }
+
+    const doc = this.parser.parseFromString(
+      documentXml.getData().toString('utf8'),
+      'text/xml'
+    );
+
+    const paragraphs = doc.getElementsByTagName('w:p');
+    return Array.from(paragraphs)
+      .map((p) => {
+        const texts = p.getElementsByTagName('w:t');
+        return Array.from(texts)
+          .map((t) => t.textContent)
+          .join('');
+      })
+      .join('\n');
   }
 }
